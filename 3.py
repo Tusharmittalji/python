55a1c5a68e08ab43fc189852bfc71b7d2989bd8a
tushar <tusharmittal1995@gmail.com>
2023-08-20 10:59:26 +0530

    Second commit

---
+++ 3.py
@@ -0,0 +1,10 @@
+number1= int(input("enter your first number = "))
+number2 = int(input("enter your secpind number = "))
+
+add= number1+number2
+
+print ("the sum of ", number1 ,"and", number2, "is", add )
+
+sub= number1-number2
+
+print ("the substraction  of ", number1 ,"and", number2, "is", sub )
